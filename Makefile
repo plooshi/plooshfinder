2c20fa978f7e11e1b6af8985a1c4a6b36ef9f210
Tom E <guacaplushy@icloud.com>
2023-03-11 08:30:47 -0500

    Init plooshfinder

---
+++ Makefile
@@ -0,0 +1,25 @@
+INCLDIRS = -I./include
+SRC = $(wildcard src/*)
+OBJDIR = obj
+OBJS = $(patsubst src/%,$(OBJDIR)/%,$(SRC:.c=.o))
+
+LDFLAGS ?= -fuse-ld=lld
+CC := clang
+AR ?= ar
+RANLIB ?= ranlib
+
+all: dirs $(OBJS) libplooshfinder.a
+
+dirs:
+	@mkdir -p $(OBJDIR)
+
+clean:
+	@rm -rf libplooshfinder.a obj
+
+libplooshfinder.a: $(OBJS)
+	$(AR) rcs libplooshfinder.a $(OBJS)
+	$(RANLIB) libplooshfinder.a
+
+
+$(OBJDIR)/%.o: src/%.c
+	$(CC) $(CFLAGS) $(INCLDIRS) -c -o $@ $<
---
+++ include/plooshfinder.h
@@ -0,0 +1,17 @@
+#ifndef _PLOOSHFINDER_H
+#define _PLOOSHFINDER_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void pf_find_maskmatch(void *buf, size_t size, uint32_t matches[], uint32_t masks[], uint32_t count, bool (*callback)(void* stream));
+uint32_t *pf_find_next(uint32_t *stream, uint32_t count, uint32_t match, uint32_t mask);
+uint32_t *pf_find_prev(uint32_t *stream, uint32_t count, uint32_t match, uint32_t mask);
+int32_t pf_signextend_32(int32_t val, uint8_t bits);
+int64_t pf_signextend_64(int64_t val, uint8_t bits);
+uint32_t *pf_follow_branch(uint32_t *insn);
+int64_t pf_adrp_offset(uint32_t adrp);
+void *pf_follow_xref(uint32_t *stream);
+
+#endif
