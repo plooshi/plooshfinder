aedbdb2abe3f20accf0d5036a3350e056c586cb5
Tom E <me@ploosh.dev>
2023-04-29 19:11:12 -0400

    Add ELF symbol finding

--- include/elf_defs.h
+++ include/elf_defs.h
@@ -2,6 +2,10 @@
 #define _ELF_DEFS_H
 #include <stdint.h>
 
+#define SHT_SYMTAB 0x2
+#define SHT_DYNSYM 0xb
+#define PT_LOAD 0x1
+
 struct elf_ident_64 {
     char signature[4];
     uint8_t file_class;
@@ -26,7 +30,18 @@ struct elf_header_64 {
     uint16_t ph_count;
     uint16_t sh_size;
     uint16_t sh_count;
-    uint16_t sect_table_ind;
+    uint16_t sect_table_index;
+};
+
+struct elf_pheader_64 {
+    uint32_t type;
+    uint32_t flags;
+    uint64_t offset;
+    uint64_t virtual_address;
+    uint64_t physical_address;
+    uint64_t file_size;
+    uint64_t memory_size;
+    uint64_t align;
 };
 
 struct elf_sheader_64 {
@@ -40,6 +55,15 @@ struct elf_sheader_64 {
     uint32_t info;
     uint64_t align;
     uint64_t entry_count;
+};
+
+struct elf_symbol_64 {
+    uint32_t name;
+    unsigned char info;
+    unsigned char other;
+    uint16_t sh_index;
+    uint64_t offset;
+    uint64_t size;
 };
 
 #endif
--- src/elf.c
+++ src/elf.c
@@ -11,7 +11,7 @@ bool is_elf(void *buf) {
     if (strncmp(hdr->ident.signature, elf_magic, 4) == 0) {
         return true;
     } else {
-        printf("%s: Not an ELF!\n", __FUNCTION__);
+        printf("Not an ELF!\n");
     }
 
     return false;
@@ -25,7 +25,7 @@ struct elf_sheader_64 *elf_get_section(void *buf, char *name) {
     struct elf_header_64 *hdr = (struct elf_header_64 *) buf;
     struct elf_sheader_64 *section_hdr = buf + hdr->sh_off;
 
-    struct elf_sheader_64 *sname_hdr = section_hdr + hdr->sect_table_ind;
+    struct elf_sheader_64 *sname_hdr = section_hdr + hdr->sect_table_index;
     void *sname_tbl = buf + sname_hdr->offset;
     
 
@@ -40,4 +40,85 @@ struct elf_sheader_64 *elf_get_section(void *buf, char *name) {
 
     printf("%s: Unable to find section %s!\n", __FUNCTION__, name);
     return NULL;
+}
+
+uint64_t elf_get_offset(void *buf) {
+    if (!is_elf(buf)) {
+        return 0;
+    }
+
+    struct elf_header_64 *hdr = (struct elf_header_64 *) buf;
+    struct elf_pheader_64 *program_hdr = buf + hdr->ph_off;
+
+    for (int i = 0; i < hdr->ph_count; i++) {
+        struct elf_pheader_64 *phdr = program_hdr + i;
+
+        if (phdr->type == PT_LOAD) {
+            return phdr->virtual_address;
+        }
+    }
+
+    return 0;
+}
+
+struct elf_symbol_64 *elf_find_symbol_stype(void *buf, char *name, uint32_t type) {
+    if (!is_elf(buf)) {
+        return NULL;
+    }
+
+    struct elf_header_64 *hdr = (struct elf_header_64 *) buf;
+    struct elf_sheader_64 *section_hdr = buf + hdr->sh_off;
+    struct elf_sheader_64 *sect = NULL;
+    struct elf_sheader_64 *strtab_sect;
+
+    for (uint16_t i = 0; i < hdr->sh_count; i++) {
+        struct elf_sheader_64 *section = section_hdr + i;
+
+        if (section->type == type) {
+            sect = section;
+        }
+    }
+
+    if (!sect) {
+        return NULL;
+    }
+
+    strtab_sect = section_hdr + sect->link;
+
+    struct elf_symbol_64 *symtab = buf + sect->offset;
+    char *strtab = buf + strtab_sect->offset;
+    uint64_t count = sect->size / sizeof(struct elf_symbol_64);
+
+    for (int i = 0; i < count; i++) {
+        struct elf_symbol_64 *symbol = symtab + i;
+        char *sym_name = strtab + symbol->name;
+
+        if (strcmp(sym_name, name) == 0) {
+            uint64_t offset = elf_get_offset(buf);
+
+            symbol->offset = symbol->offset - offset;
+            return symbol;
+        }
+    }
+
+    return NULL;
+}
+
+struct elf_symbol_64 *elf_find_symbol(void *buf, char *name) {
+    if (!is_elf(buf)) {
+        return NULL;
+    }
+
+    struct elf_symbol_64 *symbol = elf_find_symbol_stype(buf, name, SHT_SYMTAB);
+
+    if (!symbol) {
+        symbol = elf_find_symbol_stype(buf, name, SHT_DYNSYM);
+    }
+
+    if (!symbol) {
+        printf("%s: Failed to find symbol %s!\n", __FUNCTION__, name);
+        return NULL;
+    }
+
+    return symbol;
 }
