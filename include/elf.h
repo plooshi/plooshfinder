79b64966d9760d43ef63627faf8ef4526cd70375
Tom E <me@ploosh.dev>
2023-04-29 19:21:43 -0400

    add new functions to header

--- include/elf.h
+++ include/elf.h
@@ -5,5 +5,8 @@
 
 bool is_elf(void *buf);
 struct elf_sheader_64 *elf_get_section(void *buf, char *name);
+uint64_t elf_get_offset(void *buf);
+struct elf_symbol_64 *elf_find_symbol_stype(void *buf, char *name, uint32_t type);
+struct elf_symbol_64 *elf_find_symbol(void *buf, char *name);
 
 #endif
